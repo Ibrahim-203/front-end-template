5b319e983ac7d966dc351ebfa99154c9d98f4c84
Ibrahim <you@example.com>
2026-03-25 10:59:58 +0300

    Adding private and public route.

--- Admin-1/src/App.jsx
+++ Admin-1/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth} from "./contexts/AuthContext";
+import { AuthProvider} from "./contexts/AuthContext";
 
 import AdminLayout from "./components/layout/AdminLayout";
 import LoginPage from "./pages/LoginPage";
@@ -7,44 +7,32 @@ import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import UsersPage from "./pages/UsersPage";
 import SettingsPage from "./pages/SettingsPage";
-
-function AppRoutes(){
-  const {isLoggedIn, loading} = useAuth();
-
-  if(loading){
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-[#f8fafc]">
-        <div className="text-[#64748b]">Chargement...</div>
-      </div>
-    )
-  }
-
-  return (
-    <Routes>
-      {!isLoggedIn ? (
-        <>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </>
-      ) : (
-        <Route path="/" element={<AdminLayout />}>
-          <Route index element={<DashboardPage />} />
-          <Route path="users" element={<UsersPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      )}
-    </Routes>
-  );
-}
+import PrivateRoute from "./components/routing/PrivateRoute";
+import PublicRoute from "./components/routing/PublicRoute";
 
 export default function App() {
-
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {/* Route publique : Login */}
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+
+          {/* Routes protégées */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<AdminLayout />}>
+              <Route index element={<DashboardPage />} />
+              <Route path="users" element={<UsersPage />} />
+              <Route path="settings" element={<SettingsPage />} />
+            </Route>
+          </Route>
+
+          {/* Route de fallback */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
